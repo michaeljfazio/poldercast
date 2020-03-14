526d14bfc0079f7fe49475ae76483ca48bdc6fa5
Michael Fazio <michaelfazio@me.com>
2020-03-14 16:04:51 +0800

    Added preferred peers custom layer

--- src/custom_layers/mod.rs
+++ src/custom_layers/mod.rs
@@ -1,3 +1,5 @@
+mod preferred_peers;
 mod random_direct_connections;
 
+pub use self::preferred_peers::PreferredPeers;
 pub use self::random_direct_connections::RandomDirectConnections;
---
+++ src/custom_layers/preferred_peers.rs
@@ -0,0 +1,46 @@
+use crate::{GossipsBuilder, Id, Layer, NodeProfile, Nodes, ViewBuilder};
+
+#[derive(Clone, Debug)]
+pub struct PreferredPeers {
+    preferred: Vec<Id>,
+}
+
+impl PreferredPeers {
+    pub fn add(&mut self, id: Id) {
+        self.preferred.push(id);
+    }
+}
+
+impl Default for PreferredPeers {
+    fn default() -> Self {
+        PreferredPeers {
+            preferred: Vec::new(),
+        }
+    }
+}
+
+impl Layer for PreferredPeers {
+    fn alias(&self) -> &'static str {
+        "preferred_peers"
+    }
+
+    fn reset(&mut self) {
+        // No-op
+    }
+
+    fn populate(&mut self, _: &NodeProfile, _: &Nodes) {
+        // No-op
+    }
+
+    fn view(&mut self, view: &mut ViewBuilder, all_nodes: &mut Nodes) {
+        self.preferred.iter().for_each(|id| {
+            if let Some(node) = all_nodes.get_mut(id) {
+                view.add(node);
+            }
+        })
+    }
+
+    fn gossips(&mut self, _: &NodeProfile, _: &mut GossipsBuilder, _: &Nodes) {
+        // We won't gossip preferred peers. Instead we rely on them to perform their own gossiping.
+    }
+}
